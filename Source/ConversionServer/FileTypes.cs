f30c73b27ce82af66903c21521576b8f182560c9
Giuseppe Silvano <giuseppe.silvano@gmail.com>
2015-10-06 14:36:25 +0200

    Use incremental enum values

--- Source/ConversionServer/FileTypes.cs
+++ Source/ConversionServer/FileTypes.cs
@@ -5,50 +5,50 @@
 
         // Word formats
 
-        doc = 1,
-        dot = 2, // Legacy Word Template
+        doc,
+        dot, // Legacy Word Template
 
-        docx = 3,
-        docm = 4,
+        docx,
+        docm,
 
-        dotx = 5,
-        dotm = 6,
+        dotx,
+        dotm,
 
-        rtf = 7,
+        rtf,
        
         // Excel formats
 
-        xls = 8,
-        xlt = 9, // Legacy Excel Template
+        xls,
+        xlt, // Legacy Excel Template
 
-        xlsx = 10,
-        xlsm = 11,
+        xlsx,
+        xlsm,
 
-        xltm = 12,
-        xltx = 13,
+        xltx,
+        xltm,
 
         // Powerpoint formats
 
-        ppt = 14,
-        pps = 15, // Legacy PowerPoint slideshow
-        pot = 16, // Legacy PowerPoint Template
+        ppt,
+        pps, // Legacy PowerPoint slideshow
+        pot, // Legacy PowerPoint Template
 
-        pptx = 17,
-        pptm = 18,
+        pptx,
+        pptm,
 
-        ppsx = 19,
-        ppsm = 20,
+        ppsx,
+        ppsm,
 
-        potx = 21,
-        potm = 22,
+        potx,
+        potm,
 
         // PDF & OCR
 
-        pdf = 23,
-        bmp = 24,
-        gif = 25,
-        png = 26,
-        jpeg = 27,
-        tiff = 28
+        pdf,
+        bmp,
+        gif,
+        png,
+        jpeg,
+        tiff
     }
 }
